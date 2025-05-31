431aee289b3a51d320f9eb4bf4a93dcc1f9ced55
brendayw <brendayw97@gmail.com>
2025-05-31 19:41:09 -0300

    fix en endpoint sessions by patient id

--- src/routes/sessionRoutes.js
+++ src/routes/sessionRoutes.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.post('/session', checkJwt, logSession);
 // router.get('/session', checkJwt, logSession);
-router.get('/patients/sessions/:id', checkJwt, getSessionsByPatient);
+router.get('/patients/:id/sessions', checkJwt, getSessionsByPatient);
 router.get('/session/:patientId/last', checkJwt, getLastSessionForPatient);
 router.put('/session/:sessionId', checkJwt, updateSession);
 router.delete('/session/:sessionId', checkJwt, deleteSession);
